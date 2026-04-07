b2789ea2e1c3162725d91122e6f4bf8b2106940e
Aaditya Bajgain <adityabajgain@gmail.com>
2026-04-06 23:31:43 -0700

    Refactor stack class methods for clarity and consistency

--- stack/stackWithList/staack.py
+++ stack/stackWithList/staack.py
@@ -10,15 +10,32 @@ class stack:
     def __len__(self):
         return len(self.items)
     
+    def is_empty(self):
+        return len(self.items) == 0
+    
     def __str__(self):
-        if self.is_empty:
+        if self.is_empty():
             return "stack is empty"
         values = [str(value) for value in reversed(self.items)]
         return "\n".join(values)
     
-    def is_empty(self):
-        return len(self.items) == 0
     
+    def pop(self):
+        if self.is_empty():
+            return "Stack is empty"
+        return self.items.pop()
+        
+        
+    def peek(self):
+        if self.is_empty():
+            return "Stack is empty"
+        return self.items[-1]
+    
+    def size(self):
+        return len(self.items)
+    
+    def clear(self):
+        self.items = []
 my_stack = stack()
 
 my_stack.push(10)
@@ -27,5 +44,10 @@ my_stack.push(30)
 my_stack.push(40)
 
 print(my_stack.items)
-print(len(my_stack))
+# print(len(my_stack))
 print(my_stack)
+print(my_stack.pop())
+
+print("-"*50)
+print(my_stack)
+print(my_stack.peek())
